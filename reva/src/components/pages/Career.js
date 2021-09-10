7bdc0b51b7f350d388d697396c2f9faf714c09c8
Poorva Joshi <56001271+poorvajoshi1305@users.noreply.github.com>
2021-09-10 15:21:22 +0530

    Add files via upload

---
+++ reva/src/components/pages/Career.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function career() {
+  return <h1 className='career'>CAREER</h1>;
+}
---
+++ reva/src/components/pages/Clients.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function clients() {
+  return <h1 className='clients'>CLIENTS</h1>;
+}
